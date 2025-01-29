e1ad071621198592297e29ebb6d06f492a341e5a
aly.shmahell <aly.shmahell@gmail.com>
2025-01-29 01:24:33 +0100

    preparing to merge

--- install.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if __name__ == '__main__':
-    os.system("cmake ./")
-    os.system("sudo cmake --build . --target install")
-    os.system("rm -rf CMakeCache.txt install_manifest.txt Makefile CMakeFiles cmake_install.cmake")
